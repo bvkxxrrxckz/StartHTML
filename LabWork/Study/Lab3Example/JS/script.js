d286d4fd9ba9db2ce756d070b53f2496e1d1ab70
tselad <aleksei.tselitsky@gmail,com>
2021-06-18 13:11:11 +0700

    Начал выполнять 3 лабораторную работу.

---
+++ LabWork/Study/Lab3Example/JS/script.js
@@ -0,0 +1,12 @@
+﻿function showMessage ()
+{
+    alert("Вы щелкнули по ДИВ");
+}
+
+function areaRectangle(obj)
+{
+    var a=obj.t1.value;
+    var b=obj.t2.value;
+    var s=a*b;
+    obj.result.value=s;
+}
---
+++ LabWork/Study/Lab3Example/JS/script1.js
@@ -0,0 +1,12 @@
+﻿function message (m)
+{
+    alert(m);
+}
+function showDesc (obj, n)
+{
+    obj.desc.value = n;
+}
+function delet (obj)
+{
+    obj.desc.value=' ';
+}
